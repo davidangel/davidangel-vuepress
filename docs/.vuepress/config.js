6ffb79e9c570e70bd6394f393684a000728b227f
davidangel <david@davidangel.net>
2019-02-14 23:05:32 -0500

    Add navbar items.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,20 @@
 module.exports = {
     title: 'David Angel',
     description: 'Software Engineer',
-    permalink: '/:year/:month/:day/:slug'
+    permalink: '/:year/:month/:day/:slug',
+    themeConfig: {
+        nav: [{
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'Resume',
+                link: '/resume/'
+            },
+            {
+                text: 'Contact',
+                link: '/contact/'
+            },
+        ]
+    }
 }
