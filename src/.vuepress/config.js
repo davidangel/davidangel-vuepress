8d16bb1d501e97e60522895d773c0858db70d780
davidangel <david@davidangel.net>
2019-02-18 21:26:03 -0500

    Eject theme.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
     title: 'David Angel',
     description: 'Software Engineer',
     permalink: '/:year/:month/:day/:slug',
+    dest: './public',
     themeConfig: {
         nav: [{
                 text: 'Home',
@@ -16,5 +17,23 @@ module.exports = {
                 link: '/contact/'
             },
         ]
-    }
+    },
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                ga: '' // UA-00000000-0
+            }
+        ],
+        [
+            'vuepress-plugin-rss',
+            {
+                base_url: '/',
+                site_url: '',
+                filter: frontmatter => frontmatter.date <= new Date(currentDateUTC),
+                count: 20
+            }
+        ],
+        'vuepress-plugin-janitor'
+    ],
 }
---
+++ src/uses.md
@@ -0,0 +1,26 @@
+---
+date: 2019-02-15 03:13:30 +0000
+title: davidangel / uses
+permalink: /uses
+---
+
+# davidangel / uses
+
+## Software
+
+I've been doing a lot of code editing in VSCode, but occasionally switch back to PHPStorm when I want better PHP debugging, or even Sublime Text if I want to quickly edit some text files. My primary terminal app is iTerm, but I'm also dabbling with Hyper, lately.
+
+## Learning Resources
+
+I'm a lifetime member to Laracasts, an excellent resource for learning Laravel, PHP, VueJS, and a lot more.
+
+## Hardware
+
+- Macbook Pro (circa 2015)
+- Logitech G513 Keyboard
+- Logitech G502 Mouse
+
+## Mobile
+
+- iPhone 7 Plus
+- Apple Watch 4
