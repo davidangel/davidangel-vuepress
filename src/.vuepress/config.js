b1cacbcb790098cb6441d465d7791ca6b7d9582e
David Angel <david@davidangel.net>
2019-10-28 21:50:23 -0400

    Add sitemap generation.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,35 +1,41 @@
 module.exports = {
-    title: 'David Angel',
-    description: 'My name is David Angel. I’m a web developer with a passion for finding efficient ways to solve problems.',
-    permalink: '/:year/:month/:day/:slug',
-    dest: './dist',
-    themeConfig: {
-        nav: [{
-                text: 'Home',
-                link: '/'
-            },
-            {
-                text: 'Resume',
-                link: '/resume/'
-            },
-            {
-                text: 'Contact',
-                link: '/contact/'
-            },
-        ]
-    },
-    postcss: {
-        plugins: [
-            require("tailwindcss")("./tailwind.config.js"),
-            require("autoprefixer")
-        ]
-    },
+  title: "David Angel",
+  description:
+    "My name is David Angel. I’m a software engineer with a passion for finding efficient ways to solve problems.",
+  permalink: "/:year/:month/:day/:slug",
+  dest: "./dist",
+  themeConfig: {
+    nav: [
+      {
+        text: "Home",
+        link: "/"
+      },
+      {
+        text: "Resume",
+        link: "/resume/"
+      },
+      {
+        text: "Contact",
+        link: "/contact/"
+      }
+    ]
+  },
+  postcss: {
     plugins: [
-        [
-            '@vuepress/google-analytics',
-            {
-                ga: 'UA-5083765-21'
-            }
-        ]
-    ],
+      require("tailwindcss")("./tailwind.config.js"),
+      require("autoprefixer")
+    ]
+  },
+  plugins: [
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-5083765-21"
+      },
+      "sitemap",
+      {
+        hostname: "https://davidangel.net"
+      }
+    ]
+  ]
 }
