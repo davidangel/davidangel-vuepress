03f1d5bb238907ddbc0f5d9a31e01046963b5d3b
David Angel <david@davidangel.net>
2020-01-29 22:19:29 -0500

    WIP

---
+++ src/.vuepress/theme/global-components/ResumePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-html="markdown($frontmatter.intro)"></div>
+    <ResumeItem
+      v-for="item in $frontmatter.resumeItems"
+      :title="item.title"
+      :companyName="item.companyName"
+      :dates="item.dates"
+      :image="item.image"
+    >
+      <div v-html="markdown(item.description)"></div>
+    </ResumeItem>
+    <div v-html="markdown($frontmatter.outro)"></div>
+  </div>
+</template>
+
+<script>
+import marked from "marked"
+
+export default {
+  name: "ResumePage",
+  methods: {
+    markdown: function(value) {
+      return marked(value, { sanitize: true })
+    }
+  }
+}
+</script>
+
+<style scoped></style>
